d0bb802224173f603cc8b327cbb6c20de4f63980
Mynhardt Burger <mynhardt@gmail.com>
2023-04-30 15:55:06 -0400

    Remove tutorial info

--- pages/index.js
+++ pages/index.js
@@ -18,10 +18,6 @@ export default function Home({ allPostsData }) {
           functionality at CheckSec. My current learning focus is JS and the web
           dev process.
         </p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{" "}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
